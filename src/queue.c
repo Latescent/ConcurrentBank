e8fc3371ab35f25b338ee8bd9f1da0be9261caf1
Latescent <neutralobs@gmail.com>
2026-06-26 11:27:22 +0330

    queue_impl

---
+++ src/queue.c
@@ -0,0 +1,50 @@
+#include "../include/queue.h"
+
+void queue_init(RequestQueue *q) {
+  q->front = 0;
+  q->rear = 0;
+  q->count = 0;
+
+  pthread_mutex_init(&q->mutex, NULL);
+
+  pthread_cond_init(&q->not_empty, NULL);
+  pthread_cond_init(&q->not_full, NULL);
+}
+
+void enqueue(RequestQueue *q, TransferRequest request) {
+  pthread_mutex_lock(&q->mutex);
+
+  while (q->count == MAX_QUEUE_SIZE) {
+    pthread_cond_wait(&q->not_full, &q->mutex);
+  }
+
+  q->requests[q->rear] = request;
+
+  q->rear = (q->rear + 1) % MAX_QUEUE_SIZE;
+
+  q->count++;
+
+  pthread_cond_signal(&q->not_empty);
+
+  pthread_mutex_unlock(&q->mutex);
+}
+
+TransferRequest dequeue(RequestQueue *q) {
+  pthread_mutex_lock(&q->mutex);
+
+  while (q->count == 0) {
+    pthread_cond_wait(&q->not_empty, &q->mutex);
+  }
+
+  TransferRequest request = q->requests[q->front];
+
+  q->front = (q->front + 1) % MAX_QUEUE_SIZE;
+
+  q->count--;
+
+  pthread_cond_signal(&q->not_full);
+
+  pthread_mutex_unlock(&q->mutex);
+
+  return request;
+}
