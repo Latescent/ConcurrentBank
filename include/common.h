145def0d7ab207ed4095f896f062e1d9ffe85f77
Latescent <neutralobs@gmail.com>
2026-06-26 10:35:11 +0330

    common_defs

--- include/common.h
+++ include/common.h
@@ -1,16 +1,31 @@
 #ifndef COMMON_H
 #define COMMON_H
 
+#include <pthread.h>
+
 #define FIFO_PATH "/tmp/bank_requests"
 
 #define NUM_ACCOUNTS 10
 #define INITIAL_BALANCE 1000
 
+#define NUM_WORKERS 4
+#define MAX_QUEUE_SIZE 100
+
+#define MAX_CUSTOMERS 5
+#define REQUESTS_PER_CUSTOMER 20
+
+typedef enum { TRANSFER, DEPOSIT, WITHDRAW, BALANCE, SHUTDOWN } RequestType;
+
 typedef struct {
+  RequestType type;
+
   int customer_id;
+
   int from_account;
   int to_account;
+
   int amount;
+
 } TransferRequest;
 
 #endif
