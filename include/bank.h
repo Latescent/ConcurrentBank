c39ed88191ca16c128037373c12891fe523652b3
Latescent <neutralobs@gmail.com>
2026-06-26 10:39:31 +0330

    bank_definitions

--- include/bank.h
+++ include/bank.h
@@ -1,12 +1,28 @@
 #ifndef BANK_H
 #define BANK_H
 
-#include <pthread.h>
+#include "common.h"
 
 typedef struct {
   int id;
   int balance;
+
   pthread_mutex_t lock;
+
 } Account;
+
+void init_bank();
+
+int transfer_money(int from, int to, int amount);
+
+void deposit_money(int account, int amount);
+
+int withdraw_money(int account, int amount);
+
+int get_balance(int account);
+
+void print_bank();
+
+int total_balance();
 
 #endif
